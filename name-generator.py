102a2326f013d6a3d87cabcf29af5dce52a623c3
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-12-03 07:27:59 -0500

    Update name-generator.py

--- name-generator.py
+++ name-generator.py
@@ -1,50 +1,51 @@
 from random import choice
-import sys
 class Namegenerator:
     def __init__(self):
-        self.prefixes=[]
-        self.sufixes=[]
-    def getting_prefixes_sufixes_values(self):
+        self.prefix=[]
+        self.suffix=[]
+    def getting_prefix_suffix_values(self):
         print(f"Name generator...!")
         for value in range(1,6):
-            getting_prefixes=input(f"Enter prefix_value{value}: ")
-            while not getting_prefixes.isalpha():
+            getting_prefix=input(f"Enter prefix_value{value}: ")
+            while not getting_prefix.isalpha():
                 print(f"Enter a valid input..")
-                getting_prefixes=input(f"Enter prefix_value{value}: ")
-                self.prefixes.append(getting_prefixes)
-            self.prefixes.append(getting_prefixes)
+                getting_prefix=input(f"Enter prefix_value{value}: ")
+                self.prefix.append(getting_prefix)
+            self.prefix.append(getting_prefix)
           
-            getting_sufixes=input(f"Enter sufixes_value{value}: ")
-            while not getting_sufixes.isalpha():
+            getting_suffix=input(f"Enter suffix_value{value}: ")
+            while not getting_suffix.isalpha():
                 print(f"Enter a valid input..")
-                getting_sufixes=input(f"Enter sufixes_value{value}: ")
-                self.sufixes.append(getting_sufixes)
-            self.sufixes.append(getting_sufixes)
+                getting_suffix=input(f"Enter suffix_value{value}: ")
+                self.suffix.append(getting_suffix)
+            self.suffix.append(getting_suffix)
                     
     def name_generating(self):
-        prefixes=choice(self.prefixes)
-        sufixes=choice(self.sufixes)
-        generated_name=prefixes+sufixes
-        return f"{val}. {generated_name}"
-obj=Namegenerator()
-obj.getting_prefixes_sufixes_values()
-print(f"Generated name..")
-for val in range(1,6):
-    print(obj.name_generating())
-getting_choice=input("Do you want to  continue this funny  name generator..[yes\no]")
-if getting_choice.casefold()=="yes":
+        prefix=choice(self.prefix)
+        suffix=choice(self.suffix)
+        generated_name=prefix+suffix
+        return f"{generated_name}"
+def continue_choice():
     obj=Namegenerator()
-    obj.getting_prefixes_sufixes_values()
+    obj.getting_prefix_suffix_values()
+    print(f"Generated name..")
     for val in range(1,6):
         print(obj.name_generating())
-elif getting_choice.casefold()=="no":
-    print("name generator is going to exit....")
-    sys.exit()
-else:
-    print("Enter a  valid input")
-    sys.exit()
-    
-            
-            
-            
+    while True:
+        getting_choice=input("Do you want to  continue this funny  name generator..[yes\no]")
+        if getting_choice.casefold()=="yes":
+            obj=Namegenerator()
+            obj.getting_prefix_suffix_values()
+            for val in range(1,6):
+                print(obj.name_generating())
+            break
+        elif getting_choice.casefold()=="no":
+            print("name generator is going to exit....")
+            exit()
+        else:
+            print("Enter a  valid input")
+continue_choice()
+                    
+                
+                
         
